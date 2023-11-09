7280f103d779382211fb99b19d347d11eb1dabd0
timothee09 <53088184+timothee09@users.noreply.github.com>
2023-11-09 16:47:00 +0100

    nom

---
+++ name.py
@@ -0,0 +1,18 @@
+name = {}
+
+
+
+for i in range (8) :
+    if i=='Chirac':
+        name["Chirac"] = Jacques Chirac
+    if i=='Giscard d\'Estaing':
+        name["Giscard d'Estaing"] = Jacques Chirac
+    if i==Chirac:
+        name["Chirac"] = Jacques Chirac
+    if i==Chirac:
+        name["Chirac"] = Jacques Chirac
+    if i==Chirac:
+        name["Chirac"] = Jacques Chirac
+    if i==Chirac:
+        name["Chirac"] = Jacques Chirac
+    
