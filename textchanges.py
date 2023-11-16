f1b5bb239a098fac924d3fa2e9008c232efbccb1
Masdupp <oscarmasdup@gmail.com>
2023-11-16 15:53:34 +0100

    conitnue

--- textchanges.py
+++ textchanges.py
@@ -1,16 +1,11 @@
-def maj_changes(directory, extension):
+import os
 
-
-
-
-
-
-
-
-
-
-
-
+def modify_text_file_to_lowercase(file_path: str, new_folder_path: str, new_file_name: str) -> None:
+    with open(file_path, 'r') as file:
+        text = file.read().lower()
+    new_file_path = os.path.join(new_folder_path, new_file_name)
+    with open(new_file_path, 'w') as file:
+        file.write(text)
 lowercase_letter = "abcdefghijklmnopqrstuvwxyzüéâäåçêëèïîìôöòûùÿáíóúñ"
 def ponct_changes(file):
     f = open(file, "w")
