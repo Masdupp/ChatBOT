aae21f85529515608ec3e9dbdff93fd7aeae8976
Masdupp <oscarmasdup@gmail.com>
2023-12-18 23:22:35 +0100

    put the good directory

--- textchanges.py
+++ textchanges.py
@@ -10,9 +10,7 @@ def modify_text_file_to_lowercase(file_path: str, new_folder_path: str, new_file
     new_file_path = os.path.join(new_folder_path, new_file_name)
     with open(new_file_path, 'w') as file:
         file.write(text)
-#text_to_modify = name.list_of_files(name.folder,"txt")
-#for j in text_to_modify:
-#    modify_text_file_to_lowercase(f"./speeches/{j}","./cleaned",j)
+
 
 lowercase_letter = "abcdefghijklmnopqrstuvwxyzüéàâäåçêëèïîìôöòûùÿáíóúñ1234567890"
 def ponct_changes(file_path: str):
@@ -25,12 +23,6 @@ def ponct_changes(file_path: str):
             res += " "
     with open(file_path, 'w') as file:
         file.write(res)
-
-
-#text_to_modifys = name.list_of_files(name.folder,"txt")
-#for j in text_to_modifys:
-#    ponct_changes(f"./cleaned/{j}")
-
 
 def tf(string): #measures how often a word appears in a specific document
     dict = {}
@@ -55,7 +47,7 @@ def tf(string): #measures how often a word appears in a specific document
 def idf(directory):  #measures the importance of a word in the entire corpus of documents
     files_names, types, dic = [], ".txt", {}
     word = ""
-    directory = r"speeches" + "\\" + directory
+    directory = r"C:\Users\Administrateur\OneDrive - Efrei\Bureau\ChatBOT\cleaned" + "\\" + directory
     for namefile in os.listdir(directory):
         if namefile.endswith(types):
             with open(directory + "\\" + namefile, 'r', encoding = 'utf-8') as file:
