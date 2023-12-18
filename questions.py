abf40ad99543c0668c297896c8a7655ec03859fd
Masdupp <oscarmasdup@gmail.com>
2023-12-18 21:24:47 +0100

    almost finished function

--- questions.py
+++ questions.py
@@ -14,9 +14,9 @@ def find_common_terms(question, document):
     common_terms = question_words.intersection(corpus_words)
     return common_terms
 
-def calculate_tf(question, corpus): #calculate the tf score of the question
+def calculate_tf(question, document): #calculate the tf score of the question
     question_words = words_from_words(question)
-    corpus_words = set(corpus.split())
+    corpus_words = set(document.split())
     tf_scores = {}
     for word in question_words:
         if word in corpus_words:
@@ -26,9 +26,9 @@ def calculate_tf(question, corpus): #calculate the tf score of the question
     return tf_scores
 
 
-def calculate_tfidf(question, corpus_directory): #associate an idf score to the words of the question
-    tf_scores = calculate_tf(question, corpus_directory)
-    idf_scores = tf_idf(corpus_directory)
+def calculate_tfidf(question, document_directory): #associate an idf score to the words of the question
+    tf_scores = calculate_tf(question, document_directory)
+    idf_scores = tf_idf(document_directory)
     tfidf_scores = {}
     for term in tf_scores:
         tfidf_scores[term] = tf_scores[term] * idf_scores[term]
@@ -52,4 +52,21 @@ def cosine_similarity(vectorA, vectorB): #returns the cosine similarity between 
     return cosine
 
 
+def find_most_relevant(tfidf_matrix, tfidf_vector, file_names): #find the most relevant document in the cleaned directory
+    similarities = cosine_similarity(tfidf_matrix, tfidf_vector)
+    most_relevant_index = similarities.argmax()
+    return file_names[most_relevant_index]
 
+def get_speeches_file_name(cleaned_file_name): #get the equivalent document from cleaned into speeches
+    return cleaned_file_name.replace("./cleaned", "./speeches")
+
+def find_keyword_and_sentence(tfidf_vector, tfidf_feature_names, document): #Locate the word with the highest TF-IDF score and locate the first occurence of the word in the relevant document, and then return the sentence containing the keyword as answer
+    keyword_index = tfidf_vector.argmax()
+    keyword = tfidf_feature_names[keyword_index]
+
+    sentences = document.split('". "')
+    for sentence in sentences:
+        if keyword in sentence:
+            return keyword, '"' + sentence + '"'
+
+    return None, None
